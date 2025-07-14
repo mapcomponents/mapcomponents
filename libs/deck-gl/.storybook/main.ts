70b5fa08ed309073e73d0249a7c43a9a52507d8a
lhackenjos <lena.hackenjos@wheregroup.com>
2025-07-14 16:10:46 +0200

    fix storybook config error

--- libs/deck-gl/.storybook/main.ts
+++ libs/deck-gl/.storybook/main.ts
@@ -7,7 +7,7 @@ const config: StorybookConfig = {
 		name: '@storybook/react-vite',
 		options: {
 			builder: {
-				viteConfigPath: 'vite.config.ts',
+				viteConfigPath: 'vite.config.mts',
 			},
 		},
 	},
