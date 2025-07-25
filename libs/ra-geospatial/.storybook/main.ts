177a96cd6a6f3177332cbc11132bec1356827d19
jannik brack <jannik.brack@wheregroup.com>
2025-07-25 09:34:56 +0200

    add @storybook/addon-docs and @storybook/addons to root so every package does not need to install it
    change storybook themes

--- libs/ra-geospatial/.storybook/main.ts
+++ libs/ra-geospatial/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
-	addons: [],
+	addons: ['@storybook/addon-docs'],
 	framework: {
 		name: '@storybook/react-vite',
 		options: {
