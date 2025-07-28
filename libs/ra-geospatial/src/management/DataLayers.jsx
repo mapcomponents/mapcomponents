74bd112e842955cfc2b4ff8aea1ebb5a53d0e4fd
jannik brack <jannik.brack@wheregroup.com>
2025-07-28 10:59:46 +0200

    remove log

--- libs/ra-geospatial/src/management/DataLayers.jsx
+++ libs/ra-geospatial/src/management/DataLayers.jsx
@@ -5,8 +5,7 @@ import { DataContext } from "./DataContext";
 export default function DataLayers() {
   const dataContext = useContext(DataContext);
 
-	console.log(dataContext.data);
-  return (
+	return (
     <>
       {dataContext.data &&
         dataContext.data?.map?.((el) => <MlGeoJsonLayer layerId={el.name + '_localstorage_layer'} key={el.name} mapId='map_1' geojson={el.featureCollection} />)}
