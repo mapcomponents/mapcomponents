f3c5c9330c83767d32a17cc32412dbd5ef26878f
jannik brack <jannik.brack@wheregroup.com>
2025-09-10 09:57:23 +0200

    update cypress config and MlCreatePdfForm.cy.tsx

--- packages/react-maplibre/src/components/MlCreatePdfForm/MlCreatePdfForm.cy.tsx
+++ packages/react-maplibre/src/components/MlCreatePdfForm/MlCreatePdfForm.cy.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
 import { composeStories } from '@storybook/testing-react';
 import { mount } from '@cypress/react';
-import * as stories from './MlCreatePdfForm.stories';
+import stories from './MlCreatePdfForm.stories';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 const { ExampleConfig }: any = composeStories(stories);
 
 describe('MlCreatePdfForm Tests', () => {
 	it('Should generate and download a PDF export of the current map preview', function () {
 		if (Cypress.env('CI')) {
 			cy.log('Skipping test in CI environment');
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-expect-error
 			this.skip();
 		}
 		mount(<ExampleConfig />);
