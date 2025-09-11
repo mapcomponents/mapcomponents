42018fe6d3f6268d3d0efa2082def70fea091fce
lenaghub <lena.hackenjos@wheregroup.com>
2025-09-11 09:49:06 +0000

    Deploying to docs from  @ 328ef5da0e1d24e8dfa37b34cc3fad145756fe1f 🚀

--- docs/react-maplibre/assets/MlMultiMeasureTool.stories-DDPnaV54.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./index-BAL2wvl_.js";import{am as o}from"./style-Cp5PyTmf.js";import{m as t}from"./MapContextDecorator-uh0yiEan.js";import"./iframe-CgaKubqP.js";import"./preload-helper-D9Z9MdNV.js";import"./index-yCGPwcUf.js";const u={title:"MapComponents/MlMultiMeasureTool",component:o,argTypes:{},decorators:t},s=()=>e.jsx(e.Fragment,{children:e.jsx(o,{multiType:!0})}),r=s.bind({});r.parameters={};r.args={};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`() => {
-  return <>
-            <MlMultiMeasureTool multiType={true} />
-        </>;
-}`,...r.parameters?.docs?.source}}};const c=["ExampleConfig"];export{r as ExampleConfig,c as __namedExportsOrder,u as default};
