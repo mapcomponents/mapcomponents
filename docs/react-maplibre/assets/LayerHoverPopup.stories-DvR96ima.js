b2394640aaa17fc94e4474b46d1c5834afac21ac
lenaghub <lena.hackenjos@wheregroup.com>
2025-09-11 10:02:15 +0000

    Deploying to docs from  @ 6830e1f4771c440bc836acbdaa38123eac31e431 🚀

---
+++ docs/react-maplibre/assets/LayerHoverPopup.stories-DvR96ima.js
@@ -0,0 +1,5 @@
+import{j as r}from"./index-Bz4brbbQ.js";import{b8 as e}from"./style-Ced6O4pe.js";import{m as t}from"./MapContextDecorator-DT4JtFu2.js";import"./iframe-Cp2Egode.js";import"./preload-helper-D9Z9MdNV.js";import"./index-BmGhH8ou.js";const u={title:"Hooks/LayerHoverPopup",component:e,argTypes:{},decorators:t},p=()=>r.jsx(r.Fragment,{children:r.jsx(e,{getPopupContent:()=>"Popup content"})}),o=p.bind({});o.parameters={};o.args={};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`() => {
+  return <>
+            <LayerHoverPopup getPopupContent={() => 'Popup content'} />
+        </>;
+}`,...o.parameters?.docs?.source}}};const d=["ExampleConfig"];export{o as ExampleConfig,d as __namedExportsOrder,u as default};
