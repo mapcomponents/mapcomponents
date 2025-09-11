3837a20651f5853ddb046aea19ca2b0d90b9471b
jannik brack <jannik.brack@wheregroup.com>
2025-09-11 12:25:16 +0200

    fix: update module path in package.json and enhance Vite config for additional formats and external dependencies

--- packages/ra-geospatial/vite.config.ts
+++ packages/ra-geospatial/vite.config.ts
@@ -39,11 +39,20 @@ export default defineConfig(() => ({
 			fileName: 'index',
 			// Change this to the formats you want to support.
 			// Don't forget to update your package.json as well.
-			formats: ['es' as const],
+			formats: ['es' as const, 'cjs' as const],
 		},
+		sourcemap: true,
 		rollupOptions: {
 			// External packages that should not be bundled into your library.
-			external: ['react', 'react-dom', 'react/jsx-runtime'],
+			external: [
+				'react',
+				'react-dom',
+				'd3',
+				'sql.js',
+				/*				...Object.keys(pkg.dependencies),
+				...Object.keys(pkg.devDependencies),*/
+			],
+			input: [path.join(__dirname, 'src/index.ts')],
 		},
 	},
 }));
--- packages/react-maplibre/vite.config.ts
+++ packages/react-maplibre/vite.config.ts
@@ -38,7 +38,7 @@ export default defineConfig(() => ({
 		lib: {
 			// Could also be a dictionary or array of multiple entry points.
 			entry: 'src/index.ts',
-			name: 'react-maplibre',
+			name: '@mapcomponents/react-maplibre',
 			fileName: 'index',
 			// Change this to the formats you want to support.
 			// Don't forget to update your package.json as well.
