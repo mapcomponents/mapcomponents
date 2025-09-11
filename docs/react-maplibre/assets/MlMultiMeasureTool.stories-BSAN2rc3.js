456be01b30285452a09793669ebeea0193b92473
JannikBrack <88673060+JannikBrack@users.noreply.github.com>
2025-09-11 15:20:18 +0000

    Deploying to docs from  @ 41154b7cc7240508feb652c70ba34eb2b10059e0 🚀

--- docs/react-maplibre/assets/MlMultiMeasureTool.stories-BSAN2rc3.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./index-8CCX2DE0.js";import{am as o}from"./style-CkLx3WLz.js";import{m as t}from"./MapContextDecorator-DQi_MfhK.js";import"./iframe-CB0nhCzg.js";import"./preload-helper-D9Z9MdNV.js";import"./index-mTUkN4GW.js";const u={title:"MapComponents/MlMultiMeasureTool",component:o,argTypes:{},decorators:t},s=()=>e.jsx(e.Fragment,{children:e.jsx(o,{multiType:!0})}),r=s.bind({});r.parameters={};r.args={};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`() => {
-  return <>
-            <MlMultiMeasureTool multiType={true} />
-        </>;
-}`,...r.parameters?.docs?.source}}};const c=["ExampleConfig"];export{r as ExampleConfig,c as __namedExportsOrder,u as default};
