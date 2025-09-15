f410848ae88a1ef6e8dc40cb4b225dd798922d42
jannik brack <jannik.brack@wheregroup.com>
2025-09-15 14:51:16 +0200

    fix order in MlOrderLayers.tsx

--- packages/react-maplibre/src/components/MlOrderLayers/MlOrderLayers.tsx
+++ packages/react-maplibre/src/components/MlOrderLayers/MlOrderLayers.tsx
@@ -26,11 +26,9 @@ const MlOrderLayers = (props: MlOrderLayersProps) => {
 						} as useLayerProps['options']
 					}
 					key={layer}
-					{...(idx > 0
-						? {
-								insertBeforeLayer: props?.layerIds?.[idx - 1],
-							}
-						: { insertBeforeLayer: props.insertBeforeLayer })}
+					insertBeforeLayer={ idx < props.layerIds.length -1
+						? props?.layerIds?.[idx + 1]
+						: props.insertBeforeLayer }
 				/>
 			))}
 		</>
