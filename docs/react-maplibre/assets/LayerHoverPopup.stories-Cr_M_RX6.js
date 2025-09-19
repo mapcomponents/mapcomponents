c0e1b58902abb2f9f55ee9e73a15a315a8ffc9e8
JannikBrack <88673060+JannikBrack@users.noreply.github.com>
2025-09-19 12:29:55 +0000

    Deploying to docs from  @ 59394c835c7d7e2a8f2071b499361b93227495b0 🚀

--- docs/react-maplibre/assets/LayerHoverPopup.stories-Cr_M_RX6.js
+++
@@ -1,5 +0,0 @@
-import{j as r}from"./index--5HtZ1Gx.js";import{b8 as e}from"./style-Cd4CLVpf.js";import{m as t}from"./MapContextDecorator-jGAL-cUK.js";import"./iframe-sB680pkI.js";import"./preload-helper-D9Z9MdNV.js";import"./index-D8UhSouY.js";const u={title:"Hooks/LayerHoverPopup",component:e,argTypes:{},decorators:t},p=()=>r.jsx(r.Fragment,{children:r.jsx(e,{getPopupContent:()=>"Popup content"})}),o=p.bind({});o.parameters={};o.args={};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`() => {
-  return <>
-            <LayerHoverPopup getPopupContent={() => 'Popup content'} />
-        </>;
-}`,...o.parameters?.docs?.source}}};const d=["ExampleConfig"];export{o as ExampleConfig,d as __namedExportsOrder,u as default};
